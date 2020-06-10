9470367017be2a03db62e3d444722ba11d55506f
Souro7 <sarkarsourobrata@gmail.com>
2020-06-10 12:26:10 +0530

    Setup input validation and add POST

--- index.js
+++ index.js
@@ -15,6 +15,28 @@ app.get('/api/genres', (req, res) => {
     res.send(genres);
 });
 
+api.post('/api/genres', (req, res) => {
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    };
+    genres.push(genre);
+    res.send(genre);
+});
+
+
+
+function validateGenre(genre) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(genre, schema);
+}
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port ${port}...`));
