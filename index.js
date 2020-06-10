5396b0b4376e6b82bc0e9a7d44db0493d0cb032c
Souro7 <sarkarsourobrata@gmail.com>
2020-06-10 12:30:15 +0530

    Add endpoints for CRUD on genres

--- index.js
+++ index.js
@@ -15,7 +15,14 @@ app.get('/api/genres', (req, res) => {
     res.send(genres);
 });
 
-api.post('/api/genres', (req, res) => {
+app.get('/api/genres/:id', (req, res) => {
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('The genre with given ID not found');
+
+    res.send(genre);
+});
+
+app.post('/api/genres', (req, res) => {
     const { error } = validateGenre(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
@@ -24,6 +31,27 @@ api.post('/api/genres', (req, res) => {
         name: req.body.name
     };
     genres.push(genre);
+    res.send(genre);
+});
+
+app.put('/api/genres/:id', (req, res) => {
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('The genre with  given ID  not found.');
+
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    genre.name = req.body.name;
+    res.send(genre);
+});
+
+app.delete('/api/genres/:id', (req, res) => {
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('The genre with  given ID  not found.');
+
+    const index = genres.indexOf(genre);
+    genres.splice(index, 1);
+
     res.send(genre);
 });
 
