c6c61cdb8efafaa54e9f8f52f0ff854450fd66c5
Souro7 <sarkarsourobrata@gmail.com>
2020-06-16 00:04:34 +0530

    Use mongoose to CRUD data from MongoDB

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const genres = require('./routes/genres');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost/vidly')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(() => console.log('Error connecting to DB..'));
+
 app.use(express.json());
 app.use('/api/genres', genres);
 
